954e399572c6f0578297c25eef1e5ffeef3a6dc7
JJAYCHEN <31304335+JJAYCHEN1e@users.noreply.github.com>
2023-11-13 12:13:58 +0800

    2023-11-07

--- app/src/main/java/com/example/deadline/Greeting.kt
+++ app/src/main/java/com/example/deadline/Greeting.kt
@@ -2,11 +2,13 @@ package com.example.deadline
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import java.util.Calendar
 import com.vdurmont.emoji.EmojiManager
 import kotlin.random.Random
 
 @Composable
+@Preview
 fun Greeting() {
     val currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
     val allEmojis = EmojiManager.getAll().toList()
--- app/src/main/java/com/example/deadline/data/deadlineList.kt
+++ app/src/main/java/com/example/deadline/data/deadlineList.kt
@@ -13,16 +13,20 @@ fun deadlineList(): List<Deadline> {
     val calendar = Calendar.getInstance()
     calendar.time = Date()  // 设置当前时间
     calendar.add(Calendar.HOUR_OF_DAY, 24)  // 增加24小时
+
+    val calendar2 = Calendar.getInstance()
+    calendar2.time = Date()
+    calendar2.add(Calendar.HOUR_OF_DAY, -24)
     return listOf(
         Deadline(
             id = 1,
             title = "Deadline 1",
-            start = Date(),
+            start = calendar2.time,
             deadline = calendar.time,
             color = Color(0xFFBB86FC),
             notification = Notification(),
             state = DeadlineState.TODO,
-            ),
+        ),
         Deadline(
             id = 2,
             title = "Deadline 2",
@@ -31,6 +35,78 @@ fun deadlineList(): List<Deadline> {
             color = Color(0xFFBB86FC),
             notification = Notification(),
             state = DeadlineState.TODO,
-            ),
-        )
+        ),
+        Deadline(
+            id = 3,
+            title = "Deadline 3",
+            start = Date(),
+            deadline = calendar.time,
+            color = Color(0xFFBB86FC),
+            notification = Notification(),
+            state = DeadlineState.TODO,
+        ),
+        Deadline(
+            id = 4,
+            title = "Deadline 4",
+            start = Date(),
+            deadline = calendar.time,
+            color = Color(0xFFBB86FC),
+            notification = Notification(),
+            state = DeadlineState.TODO,
+        ),
+        Deadline(
+            id = 5,
+            title = "Deadline 5",
+            start = Date(),
+            deadline = calendar.time,
+            color = Color(0xFFBB86FC),
+            notification = Notification(),
+            state = DeadlineState.TODO,
+        ),
+        Deadline(
+            id = 6,
+            title = "Deadline 6",
+            start = Date(),
+            deadline = calendar.time,
+            color = Color(0xFFBB86FC),
+            notification = Notification(),
+            state = DeadlineState.TODO,
+        ),
+        Deadline(
+            id = 7,
+            title = "Deadline 7",
+            start = Date(),
+            deadline = calendar.time,
+            color = Color(0xFFBB86FC),
+            notification = Notification(),
+            state = DeadlineState.TODO,
+        ),
+        Deadline(
+            id = 8,
+            title = "Deadline 8",
+            start = Date(),
+            deadline = calendar.time,
+            color = Color(0xFFBB86FC),
+            notification = Notification(),
+            state = DeadlineState.TODO,
+        ),
+        Deadline(
+            id = 9,
+            title = "Deadline 9",
+            start = Date(),
+            deadline = calendar.time,
+            color = Color(0xFFBB86FC),
+            notification = Notification(),
+            state = DeadlineState.TODO,
+        ),
+        Deadline(
+            id = 10,
+            title = "Deadline 10",
+            start = Date(),
+            deadline = calendar.time,
+            color = Color(0xFFBB86FC),
+            notification = Notification(),
+            state = DeadlineState.TODO,
+        ),
+    )
 }
