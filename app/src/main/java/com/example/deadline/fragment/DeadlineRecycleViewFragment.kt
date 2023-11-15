11e70094cd1302850bafb6a7cbfd5f2ea807b974
Hengmi <hengmii@hotmail.com>
2023-11-15 17:19:30 -0500

    feat: 实现 fragment 跳转

--- app/src/main/java/com/example/deadline/fragment/DeadlineRecycleViewFragment.kt
+++ app/src/main/java/com/example/deadline/fragment/DeadlineRecycleViewFragment.kt
@@ -7,10 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.deadline.fragment.Greeting
 import com.example.deadline.ProjectDeadlineApplication
+import com.example.deadline.R
 import com.example.deadline.databinding.FragmentDeadlineListRecyclerViewBinding
 import com.example.deadline.viewmodels.ProjectDeadlineViewModel
 import com.example.deadline.viewmodels.ProjectDeadlineViewModelFactory
@@ -52,10 +54,19 @@ class DeadlineRecycleViewFragment : Fragment() {
 //        GlobalScope.launch(Dispatchers.IO) {
 //            deadlineAdapter.submitList(viewModel.fullDeadline())
 //        }
+
+        val binding = FragmentDeadlineListRecyclerViewBinding.bind(view)
+        binding.addDeadlineButton.setOnClickListener {
+            navigateToAddDeadlineFragment()
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun navigateToAddDeadlineFragment() {
+        findNavController().navigate(R.id.action_deadlineRecycleViewFragment_to_addDeadlineFragment)
     }
 }
