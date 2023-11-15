04d86c71f3efb2c5977d877003a059f875bb8db2
Hengmi <hengmii@hotmail.com>
2023-11-15 16:15:08 -0500

    fix: add navigation

--- app/src/main/java/com/example/deadline/MainActivity.kt
+++ app/src/main/java/com/example/deadline/MainActivity.kt
@@ -3,22 +3,17 @@ package com.example.deadline
 import DeadlineAdapter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.ui.platform.ComposeView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.deadline.data.deadlineList
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val viewById = findViewById<ComposeView>(R.id.greeting_text)
-        viewById.setContent { Greeting() }
-
-        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = DeadlineAdapter(deadlineList())
     }
 }
 
