096949af3834f7cd297f3985067eee30ec1d48dc
Hengmi <hengmii@hotmail.com>
2023-11-17 22:20:05 -0400

    Using LiveData

--- app/src/main/java/com/example/deadline/data/database/DeadlineDao.kt
+++ app/src/main/java/com/example/deadline/data/database/DeadlineDao.kt
@@ -1,10 +1,15 @@
 package com.example.deadline.data.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 
 @Dao
 interface DeadlineDao {
     @Query("SELECT * FROM deadline")
-    fun getAll(): List<Deadline>
+    fun getAll(): LiveData<List<Deadline>>
+
+    @Insert
+    fun insertDeadline(vararg deadlines: Deadline)
 }
--- app/src/main/java/com/example/deadline/viewmodels/ProjectDeadlineViewModel.kt
+++ app/src/main/java/com/example/deadline/viewmodels/ProjectDeadlineViewModel.kt
@@ -1,11 +1,10 @@
 package com.example.deadline.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.example.deadline.data.database.Deadline
 import com.example.deadline.data.database.DeadlineDao
 
 class ProjectDeadlineViewModel(private val deadlineDao: DeadlineDao) : ViewModel() {
-    fun fullDeadline(): List<Deadline> {
-        return deadlineDao.getAll()
-    }
+    val fullDeadlines : LiveData<List<Deadline>> = deadlineDao.getAll()
 }
