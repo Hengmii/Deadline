b7ef87e1fe5311f500d6f971b155d16a3876fb3f
Hengmi <hengmii@hotmail.com>
2023-12-01 21:11:42 -0500

    feat: support system alarm

--- app/src/main/java/com/example/deadline/fragment/NotificationFragment.kt
+++ app/src/main/java/com/example/deadline/fragment/NotificationFragment.kt
@@ -1,21 +1,28 @@
 package com.example.deadline.fragment
 
-import android.annotation.SuppressLint
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.RecyclerView
 import com.example.deadline.NotificationList.NotificationAdapter
 import com.example.deadline.data.NotificationTime
 import com.example.deadline.databinding.NotificationFragmentBinding
 import com.example.deadline.viewmodels.NotificationViewModel
+import java.time.LocalDateTime
 
 
 class NotificationFragment : Fragment() {
@@ -47,17 +54,84 @@ class NotificationFragment : Fragment() {
             ViewModelProvider(requireActivity()).get(NotificationViewModel::class.java)
 
         val adapter = NotificationAdapter(notificationViewModel) {}.apply {
-            submitList(NotificationTime.values().toList().sortedByDescending { it.offset })
+            submitList(notificationViewModel.allNotifications)
         }
         recyclerView.adapter = adapter
+
+//        notificationViewModel.clickedNotifications.observe(viewLifecycleOwner, Observer { notifications ->
+//            adapter.submitList(notifications.toList())
+//        })
 
         notificationViewModel.notificationCounter.observe(viewLifecycleOwner, Observer { counter ->
             binding.notificationCount.text = "(${counter})"
         })
+
+        scheduleNotification(NotificationTime.AT_DEADLINE, LocalDateTime.now())
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    val CHANNEL_ID = "channel_id_example_01"
+
+    fun scheduleNotification(notificationTime: NotificationTime, deadline: LocalDateTime) {
+        createNotificationChannel()
+
+        var builder = NotificationCompat.Builder(requireContext(), CHANNEL_ID)
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .setContentTitle("My notification")
+            .setContentText("Much longer text that cannot fit one line...")
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText("Much longer text that cannot fit one line...")
+            )
+            .setWhen(System.currentTimeMillis() + 3000)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(requireContext())) {
+            // notificationId is a unique int for each notification that you must define.
+            if (ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+
+                 ActivityCompat.requestPermissions(
+                     requireActivity(),
+                     arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                     1
+                 )
+
+                return
+            }
+            notify(1, builder.build())
+        }
+    }
+
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is not in the Support Library.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel =
+                NotificationChannel(CHANNEL_ID, "channel_name_example_01", importance).apply {
+                    description = "description_example_01"
+                }
+            // Register the channel with the system.
+            val notificationManager: NotificationManager =
+                requireActivity().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
