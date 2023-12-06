06d48ddd36ddb23ded9592cb9d553d2e80d44fda
Hengmi <hengmii@hotmail.com>
2023-12-06 15:51:03 -0500

    feat: use composable function instead of xml

--- app/src/main/java/com/example/deadline/fragment/DeadlineDetailViwe.kt
+++ app/src/main/java/com/example/deadline/fragment/DeadlineDetailViwe.kt
@@ -1,26 +1,60 @@
 package com.example.deadline.fragment
 
+import android.graphics.Color.parseColor
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
 import androidx.compose.material3.Button
-import androidx.compose.material3.Divider
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign.Companion.Center
+import androidx.compose.ui.text.style.TextAlign.Companion.End
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.room.Database
-import com.example.deadline.data.database.AppDatabase
+import androidx.fragment.app.viewModels
+import com.example.deadline.ProjectDeadlineApplication
+import com.example.deadline.data.DeadlineState
 import com.example.deadline.data.database.Deadline
+import com.example.deadline.data.database.deadlineDateTimeString
+import com.example.deadline.data.database.notificationTimes
+import com.example.deadline.data.database.remainDayHourMinuteSecond
+import com.example.deadline.data.database.startDateTimeString
+import com.example.deadline.viewmodels.ProjectDeadlineViewModel
+import com.example.deadline.viewmodels.ProjectDeadlineViewModelFactory
+import kotlinx.coroutines.delay
+import java.util.concurrent.Executors
 
 @Composable
-fun DeadlineDetailView(deadline: Deadline) {
+fun DeadlineDetailView(deadline: Deadline, completeHandler: () -> Unit, backHandler: () -> Unit) {
+    val remainDays = remember { mutableStateOf(0L) }
+    val remainHours = remember { mutableStateOf(0L) }
+    val remainMinutes = remember { mutableStateOf(0L) }
+    val remainSeconds = remember { mutableStateOf(0L) }
+
+    val marginHorizontal = 16.dp
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            val remainDayHourMinuteSecond = deadline.remainDayHourMinuteSecond()
+            remainDays.value = remainDayHourMinuteSecond[0]
+            remainHours.value = remainDayHourMinuteSecond[1]
+            remainMinutes.value = remainDayHourMinuteSecond[2]
+            remainSeconds.value = remainDayHourMinuteSecond[3]
+            delay(1000L) // 等待一秒
+        }
+    }
+
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = Color.White,
@@ -28,83 +62,221 @@ fun DeadlineDetailView(deadline: Deadline) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp)
         ) {
             Row(
                 horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = deadline.startTime,
-                    fontSize = 16.sp,
-                    color = Color.Gray
-                )
-                Text(
-                    text = deadline.title,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 24.sp,
-                    modifier = Modifier.align(Alignment.CenterVertically)
-                )
-                Text(
-                    text = deadline.deadlineTime,
-                    fontSize = 16.sp,
-                    color = Color.Gray
-                )
-            }
-            Divider(color = Color.LightGray, thickness = 1.dp)
-            // Placeholder for your countdown timer
-            Text(
-                text = "Time left",
-                fontWeight = FontWeight.Bold,
-                fontSize = 18.sp,
-                modifier = Modifier.padding(top = 16.dp)
-            )
-            // Placeholder for notifications area
-            Text(
-                text = "Notifications",
-                fontWeight = FontWeight.Bold,
-                fontSize = 18.sp,
-                modifier = Modifier.padding(top = 16.dp)
-            )
-            Text(
-                text = "There are no notifications for this deadline yet",
-                color = Color.Gray,
-                fontSize = 16.sp,
-                modifier = Modifier.padding(top = 8.dp)
-            )
-            // Placeholder for quick note area
-            Text(
-                text = "Quick note",
-                fontWeight = FontWeight.Bold,
-                fontSize = 18.sp,
-                modifier = Modifier.padding(top = 16.dp)
-            )
-            Button(
-                onClick = { /* TODO: Add your note adding logic here */ },
+                verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier
+                    .background(
+                        color = Color(parseColor("#F7F7F7"))
+                    )
+                    .padding(marginHorizontal)
                     .fillMaxWidth()
-                    .padding(top = 8.dp)
             ) {
-                Text(text = "Add a note")
-            }
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 16.dp),
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Button(
-                    onClick = { /* TODO: Complete action */ },
-                    modifier = Modifier.weight(1f)
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
                 ) {
-                    Text(text = "Complete")
+                    Row {
+                        Spacer(modifier = Modifier.weight(1f))
+                        Text(
+                            text = "Done",
+                            fontSize = 17.sp,
+                            textAlign = End,
+                            fontWeight = FontWeight.Bold,
+                            color = Color(parseColor("#555555")),
+                            modifier = Modifier
+                                .padding(marginHorizontal)
+                                .clickable {
+                                    backHandler()
+                                }
+                        )
+                    }
+                    Text(
+                        text = deadline.title,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 27.sp,
+                        textAlign = Center,
+                        modifier = Modifier
+                            .padding(64.dp)
+                            .fillMaxWidth()
+                    )
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Column {
+                            Text(
+                                text = "${deadline.startDateTimeString()}",
+                                fontSize = 16.sp,
+                            )
+                            Text(
+                                text = "Start",
+                                fontSize = 14.sp,
+                                fontWeight = FontWeight.Light,
+                                color = Color.Gray
+                            )
+                        }
+                        Column {
+                            Text(
+                                text = "${deadline.deadlineDateTimeString()}",
+                                fontSize = 16.sp,
+                            )
+                            Text(
+                                text = "Deadline",
+                                fontSize = 14.sp,
+                                fontWeight = FontWeight.Light,
+                                color = Color.Gray,
+                                modifier = Modifier.align(Alignment.End)
+                            )
+                        }
+                    }
                 }
-                Spacer(modifier = Modifier.width(16.dp))
-                Button(
-                    onClick = { /* TODO: Edit action */ },
-                    modifier = Modifier.weight(1f)
+            }
+            Column(modifier = Modifier.padding(marginHorizontal)) {
+                Text(
+                    text = "Time left",
+                    fontSize = 17.sp,
+                    fontWeight = FontWeight.Light,
+                    modifier = Modifier.padding(top = 16.dp)
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
                 ) {
-                    Text(text = "Edit")
+                    if (remainDays.value + remainHours.value + remainMinutes.value + remainSeconds.value < 0L
+                    ) {
+                        Text(
+                            text = "The deadline has passed",
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.padding(horizontal = 8.dp)
+                        )
+                    } else {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier.padding(horizontal = 8.dp)
+                        ) {
+                            Text(
+                                text = remainDays.value.toString(),
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Bold,
+                            )
+                            Text(
+                                text = "Days",
+                                fontSize = 16.sp,
+                            )
+                        }
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier.padding(horizontal = 8.dp)
+                        ) {
+                            Text(
+                                text = remainHours.value.toString(),
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Bold,
+                            )
+                            Text(
+                                text = "Hours",
+                                fontSize = 16.sp,
+                            )
+                        }
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier.padding(horizontal = 8.dp)
+                        ) {
+                            Text(
+                                text = remainMinutes.value.toString(),
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Bold,
+                            )
+                            Text(
+                                text = "Minutes",
+                                fontSize = 16.sp,
+                            )
+                        }
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier.padding(horizontal = 8.dp)
+                        ) {
+                            Text(
+                                text = remainSeconds.value.toString(),
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Bold,
+                            )
+                            Text(
+                                text = "Seconds",
+                                fontSize = 16.sp,
+                            )
+                        }
+                    }
+                }
+                Column(modifier = Modifier.padding(top = 16.dp)) {
+                    Text(
+                        text = "Notifications",
+                        fontSize = 17.sp,
+                        fontWeight = FontWeight.Light,
+                    )
+                    val notificationTimes = deadline.notificationTimes()
+                    if (notificationTimes.isNotEmpty()) {
+                        Column(modifier = Modifier.padding(top = 8.dp)) {
+                            deadline.notificationTimes()
+                                .forEach { time ->
+                                    Text(
+                                        text = time.displayText,
+                                        fontSize = 16.sp,
+                                        textAlign = Center,
+                                        modifier = Modifier
+                                            .padding(horizontal = 8.dp)
+                                            .padding(vertical = 2.dp)
+                                            .fillMaxWidth()
+                                    )
+                                }
+                        }
+                    } else {
+                        Text(
+                            text = "There are no notifications for this deadline yet",
+                            color = Color.Gray,
+                            fontSize = 14.sp,
+                            modifier = Modifier.padding(top = 8.dp)
+                        )
+                    }
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 32.dp)
+                        .padding(top = 64.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Button(
+                        onClick = {
+                            completeHandler()
+                        },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(parseColor("#E7E7E7"))
+                        ),
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text(
+                            text = "Complete",
+                            color = Color.Black
+                        )
+                    }
+//                    Spacer(modifier = Modifier.width(16.dp))
+//                    Button(
+//                        onClick = { /* TODO: Edit action */ },
+//                        colors = ButtonDefaults.buttonColors(
+//                            containerColor = Color(parseColor("#E7E7E7"))
+//                        ),
+//                        modifier = Modifier
+//                            .weight(1f)
+//                    ) {
+//                        Text(
+//                            text = "Edit",
+//                            color = Color.Black,
+//                        )
+//                    }
                 }
             }
         }
@@ -119,11 +291,11 @@ fun PreviewDeadlineDetailScreen() {
         title = "Deadline Title",
         start = "1701808416256",
         startTime = "1701808416256",
-        deadline = "1701808516256",
-        deadlineTime = "1701808516256",
+        deadline = "1701808416256",
+        deadlineTime = "1701908516256",
         color = "#FF0000",
-        notification = "10",
+        notification = "ONE_MONTH_BEFORE,ONE_WEEK_BEFORE,ONE_DAY_BEFORE",
         state = "Not Started"
     )
-    DeadlineDetailView(ddl)
+    DeadlineDetailView(ddl, {}, {})
 }
--- app/src/main/java/com/example/deadline/fragment/DetailDeadlineFragment.kt
+++ app/src/main/java/com/example/deadline/fragment/DetailDeadlineFragment.kt
@@ -7,29 +7,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.example.deadline.ProjectDeadlineApplication
+import com.example.deadline.data.DeadlineState
 import com.example.deadline.data.database.Deadline
 import com.example.deadline.databinding.FullDetailFragmentBinding
+import com.example.deadline.viewmodels.ProjectDeadlineViewModel
+import com.example.deadline.viewmodels.ProjectDeadlineViewModelFactory
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 
 class DetailDeadlineFragment : Fragment() {
     private var _binding: FullDetailFragmentBinding? = null
-
     private val binding get() = _binding!!
-
     private lateinit var deadline: Deadline
-
-    private val handler = Handler(Looper.getMainLooper())
-
-    private val updateRunnable = object : Runnable {
-        override fun run() {
-            updateLeftTime()
-            handler.postDelayed(this, 1000)
-        }
-    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -39,67 +34,33 @@ class DetailDeadlineFragment : Fragment() {
         return binding.root
     }
 
-    private fun updateLeftTime() {
-        var duration = deadline.deadlineTime.toLong() - System.currentTimeMillis()
-
-        val days = TimeUnit.MILLISECONDS.toDays(duration)
-        duration -= java.util.concurrent.TimeUnit.DAYS.toMillis(days)
-
-        val hours = TimeUnit.MILLISECONDS.toHours(duration)
-        duration -= java.util.concurrent.TimeUnit.HOURS.toMillis(hours)
-
-        val minutes = TimeUnit.MILLISECONDS.toMinutes(duration)
-        duration -= java.util.concurrent.TimeUnit.MINUTES.toMillis(minutes)
-
-        val seconds = TimeUnit.MILLISECONDS.toSeconds(duration)
-        duration -= java.util.concurrent.TimeUnit.SECONDS.toMillis(seconds)
-
-//        if (days > 0) {
-//            binding.actualTimeLeft.text = "$days days, $hours hours, $minutes minutes, $seconds seconds"
-//        } else if (hours > 0) {
-//            binding.actualTimeLeft.text = "$hours hours, $minutes minutes, $seconds seconds"
-//        } else if (minutes > 0) {
-//            binding.actualTimeLeft.text = "$minutes minutes, $seconds seconds"
-//        } else if (seconds > 0) {
-//            binding.actualTimeLeft.text = "$seconds seconds"
-//        } else {
-//            binding.actualTimeLeft.text = "The deadline has passed"
-//        }
+    private val viewModel: ProjectDeadlineViewModel by viewModels {
+        ProjectDeadlineViewModelFactory(
+            (activity?.application as ProjectDeadlineApplication).database
+                .deadlineDao()
+        )
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         deadline = DetailDeadlineFragmentArgs.fromBundle(requireArguments()).deadline
-        handler.post(updateRunnable)
+        updateComposeView()
+    }
 
-        binding.deadlineTitle.text = deadline.title
-        binding.startDate.text = convertTimestampToDate(deadline.startTime.toLong())
-        binding.deadlineDate.text = convertTimestampToDate(deadline.deadlineTime.toLong())
-
-        binding.notificationContent.text = deadline.notification
-
-        val confirmButton = binding.confirmReviewDetailButton
-        confirmButton.setOnClickListener {
-            findNavController().popBackStack()
+    fun updateComposeView() {
+        binding.composeView.setContent {
+            DeadlineDetailView(deadline = deadline, {
+                val executor = Executors.newSingleThreadExecutor()
+                executor.execute {
+                    viewModel.updateDeadlineState(deadline.id, DeadlineState.DONE.toString())
+                    Handler(Looper.getMainLooper()).post {
+                        findNavController().popBackStack()
+                    }
+                }
+            }, {
+                findNavController().popBackStack()
+            })
         }
 
-        val editButton = binding.editDeadlineButton
-        editButton.setOnClickListener {
-            val action = DetailDeadlineFragmentDirections.actionDetailDeadlineFragmentToEditDeadlineFragment(deadline)
-            findNavController().navigate(action)
-        }
     }
-
-    private fun convertTimestampToDate(timestamp: Long): String {
-        val sdf = SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault())
-        val date = Date(timestamp)
-        return sdf.format(date)
-    }
-
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
 }
